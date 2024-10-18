f5e87ec378a6538a0be84f4ab9446a90af6996d9
Taylor Owen <towenmilner@gmail.com>
2024-10-18 13:27:35 -0700

    Almost got it compiling

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,6 +1,10 @@
-import { TokenPF2e } from '@module/canvas/index.js';
-import { CombatantPF2e, EncounterPF2e } from "@module/encounter/index.ts"
-import { Agent } from './Agent.js';
+import '../types/types/foundry/index.d.ts'
+import '../types/src/global.d.ts'
+
+import type { TokenDocumentPF2e } from '@module/scene/token-document/document.d.ts';
+import { /*CombatantPF2e,*/ EncounterPF2e } from "@module/encounter/index.ts"
+import { ScenePF2e } from '@module/scene/document.js';
+//import { Agent } from './Agent.js';
 
 
 Hooks.on("init", () => {
@@ -8,14 +12,14 @@ Hooks.on("init", () => {
   //CONFIG.debug.hooks = true;
 });
 
-let agents = [];
+let agents : any[] = [];
 
 Hooks.on("combatStart", (encounterPf2e: EncounterPF2e) => {
   console.log("encounter start!");
   let npcActors = [];
   let pcActors = [];
   for (let combatant of encounterPf2e.combatants) {
-    let actor = game.scenes.active.tokens.find((token : TokenPF2e) => token.actor.id === combatant.actorId).actor;
+    let actor = game?.scenes.active?.tokens.find((token : TokenDocumentPF2e<ScenePF2e>) => token.actor.id === combatant.actorId).actor;
     if (actor?.type === "npc") {
       npcActors.push([actor, combatant.tokenId]);
     }
@@ -24,9 +28,9 @@ Hooks.on("combatStart", (encounterPf2e: EncounterPF2e) => {
     }
   }
 
-  for (const [npcActor, tokenId] of npcActors) {
-    agents.push(new Agent(npcActor, tokenId, pcActors));
-  }
+  // for (const [npcActor, tokenId] of npcActors) {
+  //   agents.push(new Agent(npcActor, tokenId, pcActors));
+  // }
 });
 
 Hooks.on("deleteCombat", () => {
